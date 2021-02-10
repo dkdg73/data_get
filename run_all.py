bd2c43e92b9e2e0a0af3d7700da5625e08ea90ae
dkdg73 <dylan.grice@googlemail.com>
2021-02-10 15:27:17 +0000

    new run_all module complete

---
+++ run_all.py
@@ -0,0 +1,14 @@
+import equity_dg
+print("equity data uptodate")
+import credit_dg
+print("credit data uptodate")
+import gov_dg
+print("gov data uptodate")
+import inflation_dg
+print("inflation data uptodate")
+import money_dg
+print("money data uptodate")
+import tips_dg
+print("TIPS data uptodate")
+
+

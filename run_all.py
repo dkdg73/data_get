0ce4b8197a8a6b2eb0f5970e876850077d6c5a17
dkdg73 <dylan.grice@googlemail.com>
2021-02-10 15:37:44 +0000

    runtimes added to run_all

--- run_all.py
+++ run_all.py
@@ -1,14 +1,30 @@
-import equity_dg
-print("equity data uptodate")
-import credit_dg
-print("credit data uptodate")
-import gov_dg
-print("gov data uptodate")
-import inflation_dg
-print("inflation data uptodate")
-import money_dg
-print("money data uptodate")
-import tips_dg
-print("TIPS data uptodate")
+import time 
 
+start = time.time()
+
+import equity_dg
+equity_time = time.time() - start
+print(f'equity data uptodate; {equity_time}s run time')
+
+import credit_dg
+credit_time = time.time() - equity_time
+print(f'credit data uptodate; {credit_time}s run time')
+
+import gov_dg
+gov_time = time.time()-credit_time
+print(f'gov data uptodate; {gov_time}s run time')
+
+import inflation_dg
+inflation_time = time.time() - gov_time
+print(f'inflation data uptodate; {inflation_time}s run time')
+
+import money_dg
+money_time = time.time() - inflation_time
+print(f'money data uptodate; {money_time}s run time')
+
+import tips_dg
+tips_time = time.time() - money_time
+print(f'TIPS data uptodate; {tips_time}s run time')
+
+print(f'Total runtime: {tips_time - start}s')
 

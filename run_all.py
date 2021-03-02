d2b9041bce4180b3a9a3f6f5d35917f7b18a01b7
dkdg73 <dylan.grice@googlemail.com>
2021-03-02 08:10:34 +0000

    run_all module run times now correct

--- run_all.py
+++ run_all.py
@@ -3,28 +3,34 @@ import time
 start = time.time()
 
 import equity_dg
-equity_time = time.time() - start
+equity_time = round(time.time() - start, 1)
 print(f'equity data uptodate; {equity_time}s run time')
 
+startcredit = time.time()
 import credit_dg
-credit_time = time.time() - equity_time
+credit_time = round(time.time() - startcredit, 1)
 print(f'credit data uptodate; {credit_time}s run time')
 
+startgov = time.time()
 import gov_dg
-gov_time = time.time()-credit_time
+gov_time = round(time.time()- startgov, 1)
 print(f'gov data uptodate; {gov_time}s run time')
 
+startinf = time.time()
 import inflation_dg
-inflation_time = time.time() - gov_time
+inflation_time = round(time.time() - startinf, 1)
 print(f'inflation data uptodate; {inflation_time}s run time')
 
+startmoney = time.time()
 import money_dg
-money_time = time.time() - inflation_time
+money_time = round(time.time() - startmoney, 1)
 print(f'money data uptodate; {money_time}s run time')
 
+starttips = time.time()
 import tips_dg
-tips_time = time.time() - money_time
+tips_time = round(time.time() - starttips, 1)
 print(f'TIPS data uptodate; {tips_time}s run time')
 
-print(f'Total runtime: {tips_time - start}s')
+total_run_time = round(time.time() - start, 1)
+print(f'Total runtime: {total_run_time}s')
 
